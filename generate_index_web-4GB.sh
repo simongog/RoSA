23f4fe663fe5c6cd186498ec47fdd5071c2b50b2
Simon Gog <simon.gog@gmail.com>
2013-08-01 21:03:51 +1000

    Cleaning up

--- generate_index_web-4GB.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-for fac_dens in 1 2 4 8 16 32 64 128 256 512 1024 0; do
-	echo "fac_dens=${fac_dens}"
-	./bin/rosa_sd2_delta --input_file=web-4GB --verbose --tmp_file_dir=tmp/ --fac_dens=${fac_dens} --generate_index
-done
