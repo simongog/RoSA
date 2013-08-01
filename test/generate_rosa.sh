b69d1cd08affe85361a4af93038efc9e8efb46fc
Simon Gog <simon.gog@gmail.com>
2013-08-01 21:09:44 +1000

    More cleanup

--- test/generate_rosa.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#../bin/rosa_sd --input_file=$1 --threshold=4000 --generate_index --tmp_file_dir=../tmp --output_dir=../data/output/indexes
-../bin/rosa_sd --input_file=$1 --threshold=40000 --generate_index --tmp_file_dir=../tmp --output_dir=../data/output/indexes
--- test/show_statistics.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-../bin/rosa_sd --input_file=$1 --threshold=4096 --output_statistics  --output_dir=../data/output/indexes
-../bin/rosa_sd --input_file=$1 --threshold=16384 --output_statistics  --output_dir=../data/output/indexes
